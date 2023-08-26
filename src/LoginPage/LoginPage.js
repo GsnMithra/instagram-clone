00e50295b87e771a5c564a6bf3f8121252eb168c
Gsn Mithra <mithra@Gsns-Mac-Pro.local>
2023-08-26 18:38:14 +0530

    added line seperator

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -3,17 +3,28 @@ import Logo from '../Resources/instagramLogo.jpg';
 import InputBox from '../InputBox/InputBox';
 
 function LoginPage () {
+    const handleClick = (event) => {
+        event.preventDefault();
+    };
+
     return (
         <div className='LoginPage'>
             <div className='loginPrompt'>
                 <div className='image'>
                     {/* <Link to='/'> */}
+                    <a href='#' onClick={handleClick}>
                         <img src={Logo} id='logoImage' alt='Instagram'></img>
+                    </a>
                     {/* </Link> */}
                 </div>
                 <div className='loginInput'>
                     <InputBox placeHolder='Phone number, username, or email'/>
                     <InputBox placeHolder='Password'/>
+                </div>
+                <div className='lineSeperator'>
+                    <hr id='leftLine'></hr>
+                    <p id='orText'>OR</p>
+                    <hr id='rightLine'></hr>
                 </div>
             </div>
             <div className='pictureView'>
