3fdb8207a9921b6fc48f43469abd763dc25f9726
Gsn Mithra <mithra@Gsns-Mac-Pro.local>
2023-08-27 15:43:01 +0530

    added login options and create account

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -1,7 +1,11 @@
 import './LoginPage.css';
+
+import FacebookIcon from '../Resources/facebookIcon.png';
 import Logo from '../Resources/instagramLogo.jpg';
+
 import InputBox from '../InputBox/InputBox';
 import Footer from '../Footer/Footer';
+
 import ImageA from '../Resources/instagramMain.png'
 import ImageB from '../Resources/instagramMain2.png'
 import ImageC from '../Resources/instagramMain3.png'
@@ -20,22 +24,38 @@ function LoginPage () {
                     <img src={ImageC} className="fade-image"></img>
                 </div>
             </div>
-            <div className='loginPrompt'>
-                <div className='image'>
-                    {/* <Link to='/'> */}
-                    <a href='#' onClick={handleClick}>
-                        <img src={Logo} id='logoImage'></img>
-                    </a>
-                    {/* </Link> */}
+            <div className='verticalStack'>
+                <div className='loginPrompt'>
+                    <div className='image'>
+                        {/* <Link to='/'> */}
+                        <a href='#' onClick={handleClick}>
+                            <img src={Logo} id='logoImage'></img>
+                        </a>
+                        {/* </Link> */}
+                    </div>
+                    <div className='loginInput'>
+                        <InputBox placeHolder='Phone number, username, or email' type='text'/>
+                        <InputBox placeHolder='Password' type='password'/>
+                    </div>
+                    <div className='loginButton'>
+                        <span>Log in</span>
+                    </div>
+                    <div className='lineSeperator'>
+                        <hr id='leftLine'></hr>
+                        <span id='orText'>OR</span>
+                        <hr id='rightLine'></hr>
+                    </div>
+                    <div className='facebookLogin'>
+                        <img src={FacebookIcon}></img>
+                        <span id='faceLogin'>Log in with Facebook</span>
+                    </div>
+                    <div className='forgotPassword'>
+                        <span>Forgot password?</span>
+                    </div>
                 </div>
-                <div className='loginInput'>
-                    <InputBox placeHolder='Phone number, username, or email' type='text'/>
-                    <InputBox placeHolder='Password' type='password'/>
-                </div>
-                <div className='lineSeperator'>
-                    <hr id='leftLine'></hr>
-                    <p id='orText'>OR</p>
-                    <hr id='rightLine'></hr>
+                <div className='createAccount'>
+                    <span id='create'>Don't have an account?</span>
+                    <span id='signup'>Sign up</span>
                 </div>
             </div>
             <Footer />
