9a9badee67209e14ea931972c8b244c1f1e7ba97
GsnMithra <mitra9812345>
2023-10-13 13:55:13 +0530

    changes

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -2,6 +2,9 @@ import './HomePage.css';
 import InstagramLogo from '../Resources/instagramLogo.jpg'
 
 import HomeIcon from '../Resources/paneInstagramMain/homeInstagram.png'
+import NotifyIcon from '../Resources/paneInstagramMain/notifyInstagram.png';
+import SearchIcon from '../Resources/paneInstagramMain/searchInstagram.png';
+import CreateIcon from '../Resources/paneInstagramMain/createInstagram.png';
 
 function HomePage () {
     return (
@@ -12,10 +15,52 @@ function HomePage () {
                         <img src={InstagramLogo} id='instaLogo' alt=''></img>
                     </div>
                     <div className='menuButtons'>
-                        <div id='button'>
+                        <div id='buttons'>
                             <div id='home'>
                                 <img src={HomeIcon} id='homeButton' alt=''></img>
                                 <span>Home</span>
+                            </div>
+                        </div>
+                        <div id='buttons'>
+                            <div id='home'>
+                                <img src={SearchIcon} id='homeButton' alt=''></img>
+                                <span>Search</span>
+                            </div>
+                        </div>
+                        <div id='buttons'>
+                            <div id='home'>
+                                <img src={HomeIcon} id='homeButton' alt=''></img>
+                                <span>Explore</span>
+                            </div>
+                        </div>
+                        <div id='buttons'>
+                            <div id='home'>
+                                <img src={HomeIcon} id='homeButton' alt=''></img>
+                                <span>Reels</span>
+                            </div>
+                        </div>
+                        <div id='buttons'>
+                            <div id='home'>
+                                <img src={HomeIcon} id='homeButton' alt=''></img>
+                                <span>Messages</span>
+                            </div>
+                        </div>
+                        <div id='buttons'>
+                            <div id='home'>
+                                <img src={NotifyIcon} id='homeButton' alt=''></img>
+                                <span>Notifications</span>
+                            </div>
+                        </div>
+                        <div id='buttons'>
+                            <div id='home'>
+                                <img src={CreateIcon} id='homeButton' alt=''></img>
+                                <span>Create</span>
+                            </div>
+                        </div>
+                        <div id='buttons'>
+                            <div id='home'>
+                                <img src={HomeIcon} id='homeButton' alt=''></img>
+                                <span>Profile</span>
                             </div>
                         </div>
                     </div>
